8a67dcfbeb02d007dc7bc666d3896a0c8c9adeed
Artem Kravchenko <atyoma.kravchenko@gmail.com>
2022-01-04 23:00:59 +0200

    add html interface

--- src/index.js
+++ src/index.js
@@ -18,19 +18,39 @@ function onFormInput(e) {
   console.log(name);
 }
 
-fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags
-`)
+const searchParams = 'name,capital,population,flags,languages';
+
+fetch(`https://restcountries.com/v3.1/name/${name}?fields=${searchParams}`)
   .then(response => {
     return response.json();
   })
-  .then(country => {
-    console.log(country);
-  })
+  .then(createCountryCard)
+
+  // const capital = countrys.map(country => country.capital);
+  // console.log(capital[0]);
   .catch(error => {
     console.log(error);
   });
 
-function renderCoutryCard(coutry) {
-  console.log('render html');
-}
+// renderCountryCard(name);
+// function renderCountryCard(name) {
+//   const countryCard =
+//     '<div class="country-title"><img src="" alt=""><h2 class ="title">${name}</h2></div><div class="coutry-card"><p class="card-text">Capital:${capital}</p><p class="card-text">Languages:${Object.values(languages)}</p><p class="card-text">Population:${population}</p></div>';
+//   refs.countryInfo.innerHTML = countryCard;
+//   console.log('render html');
+// }
 //   https://restcountries.com/v2/{service}?fields={field},{field},{field}
+
+function createCountryCard(countrys) {
+  return countrys
+    .map(({ name, capital, population, flags, languages }) => {
+      return (refs.countryInfo.innerHTML = `<div class="country-title"><img src="${
+        flags.svg
+      }" alt="${name}" width ="80"><h2 class ="title">${
+        name.official
+      }</h2></div><div class="coutry-card"><p class="card-text">Capital:${capital}</p><p class="card-text">Languages:${Object.values(
+        languages,
+      )}</p><p class="card-text">Population:${population}</p></div>`);
+    })
+    .join('');
+}
